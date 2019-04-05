4229e9c95c4100696d4e9131c5b94009336d50cd
SkyMaverick <skymaverickas@gmail.com>
2019-04-05 14:14:49 +0300

    add test application and some remake Makefile

--- Makefile
+++ Makefile
@@ -6,21 +6,40 @@ else
 	SHARED := -Wl,-soname,libkfft.so -o libkfft.so
 endif
 
+CC=gcc
 CFLAGS= -Wall -g -Dkiss_fft_scalar=double
 
-AppName=app
+DemoApp=demo
+UTestSimple=ustest
+UTestCmp=ukfcmp
+UTestCmpWP=ukfcmpwp
+UAutoTestCmp=aukfcmp
+UAutoTestCmpWP=aukfcmpwp
 
-all: lib app
+AppAll=${DemoApp} ${UTestSimple} ${UTestCmp} ${UTestCmpWP} ${UAutoTestCmp} ${UAutoTestCmpWP}
+
+all: lib ${AppAll}
+
 # ===================================
 
-app:
-	gcc ${CFLAGS} -L. -I. test/testapp.c -lkfft -lm -Wl,-rpath,. -o ${AppName}
+demo: lib
+	${CC} ${CFLAGS} -L. -I. test/demo.c -lkfft -lm -Wl,-rpath,. -o ${DemoApp}
+ustest: lib
+	${CC} ${CFLAGS} -L. -I. test/test.c -lkfft -lm -Wl,-rpath,. -o ${UTestSimple}
+ukfcmp: lib
+	${CC} ${CFLAGS} -L. -I. test/test.c -DFFTW_COMPARE -lkfft -lfftw3 -lm -Wl,-rpath,. -o ${UTestCmp}
+ukfcmpwp: lib
+	${CC} ${CFLAGS} -L. -I. test/test.c -DFFTW_COMPARE -DCHECK_WITHOUT_PLAN -lkfft -lfftw3 -lm -Wl,-rpath,. -o ${UTestCmpWP}
+aukfcmp: lib
+	${CC} ${CFLAGS} -L. -I. test/test.c -DFFTW_COMPARE -DLOG_AUTO -lkfft -lfftw3 -lm -Wl,-rpath,. -o ${UAutoTestCmp}
+aukfcmpwp: lib
+	${CC} ${CFLAGS} -L. -I. test/test.c -DFFTW_COMPARE -DCHECK_WITHOUT_PLAN -DLOG_AUTO -lkfft -lfftw3 -lm -Wl,-rpath,. -o ${UAutoTestCmpWP}
 
 lib:
 	gcc ${CFLAGS} -fPIC -c *.c  -o kfft.o
 	ar crus libkfft.a kfft.o
 	gcc -shared ${CFLAGS} $(SHARED) kfft.o
-    
+
 doc:
 	@echo "Start by reading the README file.  If you want to build and test lots of stuff, do a 'make testall'"
 	@echo "but be aware that 'make testall' has dependencies that the basic kissfft software does not."
@@ -28,12 +47,12 @@ doc:
 	@echo "of kissfft and would like to make use of its regression tests."
 
 clean:
-	rm -f kfft*.tar.gz *~ *.pyc kfft*.zip *.a *.o *.so ${AppName}
+	rm -f kfft*.tar.gz *~ *.pyc kfft*.zip *.a *.o *.so ${AppAll}
 
 asm: kfft.s
 
 kfft.s: kfft.c kfft.h _kfft_guts.h
 	[ -e kfft.s ] && mv kfft.s kfft.s~ || true
-	gcc -S kfft.c -O3 -mtune=native -ffast-math -fomit-frame-pointer -unroll-loops -dA -fverbose-asm 
+	gcc -S kfft.c -O3 -mtune=native -ffast-math -fomit-frame-pointer -unroll-loops -dA -fverbose-asm
 	gcc -o kfft_short.s -S kfft.c -O3 -mtune=native -ffast-math -fomit-frame-pointer -dA -fverbose-asm -DFIXED_POINT
 	[ -e kfft.s~ ] && diff kfft.s~ kfft.s || true
---
+++ test/test.c
@@ -0,0 +1,165 @@
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+#include <time.h>
+
+#include "kfft.h"
+#if defined FFTW_COMPARE
+    #include <fftw3.h>
+#endif
+
+#ifndef TEST_COUNT
+    #define TEST_COUNT 50
+#endif
+
+#define VALS_KFFT   0
+#define VALS_FFTW   1
+
+static double
+__kfft_test (kiss_fft_scalar*   amp_scalar,
+             kiss_fft_cpx*      tmp_buffer,
+             size_t             size)
+{
+    kiss_fft_cfg FCfg = NULL;
+#ifndef CHECK_WITHOUT_PLAN
+    clock_t t_start = clock();
+
+    FCfg = kiss_fft_config (size, 0, 0, 0, NULL, NULL);
+    if (FCfg == NULL) {
+        assert("Allocation FAIL");
+    }
+    kiss_fft (FCfg, amp_scalar, tmp_buffer);
+    kiss_fft_free(&FCfg);
+
+    return (clock() - t_start) * 1000 / CLOCKS_PER_SEC;
+#else
+
+    FCfg = kiss_fft_config (size, 0, 0, 0, NULL, NULL);
+    if (FCfg == NULL) {
+        assert("Allocation FAIL");
+    }
+    clock_t t_start = clock();
+    kiss_fft (FCfg, amp_scalar, tmp_buffer);
+    clock_t t_ret = clock();
+
+    kiss_fft_free(&FCfg);
+
+    return (t_ret - t_start) * 1000 / CLOCKS_PER_SEC;
+#endif
+}
+
+#if defined FFTW_COMPARE
+static double
+__fftw_test (double*        amp_scalar,
+             fftw_complex*  tmp_buffer,
+             size_t         size)
+{
+#ifndef CHECK_WITHOUT_PLAN
+    clock_t t_start = clock();
+    fftw_plan plan = fftw_plan_dft_r2c_1d (size, amp_scalar, tmp_buffer, FFTW_ESTIMATE);
+    fftw_execute (plan);
+    fftw_destroy_plan(plan);
+    return (clock() - t_start) * 1000 / CLOCKS_PER_SEC;
+#else
+    fftw_plan plan = fftw_plan_dft_r2c_1d (size, amp_scalar, tmp_buffer, FFTW_ESTIMATE);
+    clock_t t_start = clock();
+    fftw_execute (plan);
+    clock_t t_ret = clock();
+    fftw_destroy_plan(plan);
+    return (t_ret - t_start) * 1000 / CLOCKS_PER_SEC;
+#endif /* CHECK_WITHOUT_PLAN */
+}
+#endif /* FFTW_COMPARE */
+
+#ifndef LOG_AUTO
+static void
+__report_func (double vals [][2])
+{
+    double t_kavg = 0;
+    double t_favg = 0;
+
+    for (int i = 0; i < TEST_COUNT; i++) {
+            t_kavg += vals [i] [VALS_KFFT];
+            t_favg += vals [i] [VALS_FFTW];
+    }
+#ifndef CHECK_WITHOUT_PLAN
+    fprintf (stdout, "%s (%d - %s):\n\n", "Algoritm work time", TEST_COUNT, "checks");
+#else
+    fprintf (stdout, "%s (%d - %s):\n\n", "Algoritm work time (without initialization)", TEST_COUNT, "checks");
+#endif
+    fprintf (stdout, "%s\n", " ---STEP------KFFT-------FFTW----");
+    for (int i = 0; i < TEST_COUNT; i++) {
+        fprintf(stdout, "| %3d ms. | %8.2f | %8.2f  |\n", i, vals [i] [VALS_KFFT], vals [i] [VALS_FFTW]);
+    }
+    fprintf (stdout, "%s\n", " --------------------------------");
+    fprintf(stdout, "%s:\t%8.2f ms.\n", "Average work time for KFFT", t_kavg / TEST_COUNT);
+    fprintf(stdout, "%s:\t%8.2f ms.\n", "Average work time for FFTW", t_favg / TEST_COUNT);
+}
+
+#else
+static void
+__report_func (double vals [][2])
+{
+    double t_kavg = 0;
+    double t_favg = 0;
+
+    for (int i = 0; i < TEST_COUNT; i++) {
+            t_kavg += vals [i] [VALS_KFFT];
+            t_favg += vals [i] [VALS_FFTW];
+    }
+    for (int i = 0; i < TEST_COUNT; i++) {
+        fprintf(stdout, "%8.2f", vals [i] [VALS_KFFT]);
+    }
+    fprintf(stdout, "\n");
+    for (int i = 0; i < TEST_COUNT; i++) {
+        fprintf(stdout, "%8.2f", vals [i] [VALS_FFTW]);
+    }
+    fprintf(stdout, "\n");
+    fprintf(stdout, "%8.2f\n", t_kavg / TEST_COUNT);
+    fprintf(stdout, "%8.2f\n", t_favg / TEST_COUNT);
+}
+#endif
+
+int
+main (int argc, char* argv[])
+{
+    if (argc > 1) {
+        double ivals [TEST_COUNT][2];
+        size_t size = atoi(argv[1]);
+
+        kiss_fft_scalar* amp_scalar = malloc ((size + 1) * sizeof(kiss_fft_scalar));
+        kiss_fft_cpx* kfft_spectr = malloc ((size + 1) * sizeof(kiss_fft_cpx));
+#if defined FFTW_COMPARE
+        fftw_complex* fftw_spectr = malloc ((size + 1) * sizeof(fftw_complex));
+#endif
+
+        for (int i = 0; i < TEST_COUNT; i++) {
+            // Zero memory
+            memset (amp_scalar, 0, size * sizeof(kiss_fft_scalar));
+            memset (kfft_spectr, 0, size * sizeof(kiss_fft_cpx));
+
+            srand( (unsigned)time(NULL) );
+            for (int i = 0; i < size; i++) {
+                amp_scalar[i] = rand();
+            }
+
+            ivals [i] [VALS_KFFT] = __kfft_test (amp_scalar, kfft_spectr, size);
+#if defined FFTW_COMPARE
+            ivals [i] [VALS_FFTW] = __fftw_test ((double*)amp_scalar, fftw_spectr, size);
+#endif
+        }
+
+        free (kfft_spectr);
+#if defined FFTW_COMPARE
+        free (fftw_spectr);
+#endif
+        free (amp_scalar);
+
+        __report_func (ivals);
+
+        return 0;
+    } else {
+        fprintf (stderr, "%s\n", "Need sequense size as parameter");
+        return 1;
+    }
+}
