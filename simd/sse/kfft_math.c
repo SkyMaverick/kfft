b6ef6b35d421b2c06973c8bb23fe30edc827b9c5
SkyMaverick <skymaverickas@gmail.com>
2020-05-11 20:38:38 +0300

    remove unported functions. Maybe some add later

--- simd/sse/kfft_math.c
+++ simd/sse/kfft_math.c
@@ -3,45 +3,6 @@
 
 #include "kfft_math_intern.h"
 
-#if defined(KFFT_RADER_ALGO)
-// WARNING in kfft num - always prime. Don't check this
-uint32_t
-FUNC_SSE(kfft_math_prmn)(uint32_t num) {
-    uint32_t phi = num - 1;
-    uint32_t n = phi;
-
-    uint32_t primes[MAX_ROOTS];
-    uint32_t count = 0;
-
-    for (uint32_t i = 2; i * i <= n; i++) {
-        if (n % i == 0) {
-            primes[count++] = i;
-            while (n % i == 0)
-                n /= i;
-        }
-    }
-    if (n > 1)
-        primes[count++] = n;
-
-    for (uint32_t res = 2; res <= num; ++res) {
-        bool ok = true;
-        for (uint32_t i = 0; count > i && ok; ++i) {
-            if (FUNC_SSE(kfft_math_modpow)(res, phi / primes[i], num) == 1)
-                ok = false;
-        }
-        if (ok)
-            return res;
-    }
-    return 0;
-}
-
-uint32_t
-FUNC_SSE(kfft_math_prmni)(uint32_t a, uint32_t m) {
-    return (FUNC_SSE(kfft_math_gcd)(a, m) != 1) ? 0 : FUNC_SSE(kfft_math_modpow)(a, m - 2, m);
-}
-
-#endif /* KFFT_RADER_ALGO */
-
 void
 FUNC_SSE(kfft_math_adamar_cpx)(kfft_cpx* Fout, kfft_cpx* Fin, uint32_t size) {
     // FIXME Now primitive algorithm
@@ -53,67 +14,5 @@ FUNC_SSE(kfft_math_adamar_cpx)(kfft_cpx* Fout, kfft_cpx* Fin, uint32_t size) {
         Fi = _mm_set_pd((double)Fin[i].i, (double)Fin[i].r);
         C_MUL_SSE(T, Fo, Fi);
         _mm_store_pd((double*)&Fout[i], T);
-    }
-}
-
-void
-FUNC_SSE(kfft_math_transpose_cpx)(const kfft_cpx* Fin, kfft_cpx* Fout, const uint32_t x,
-                                  const uint32_t y) {
-    for (uint64_t n = 0; n < x * y; n++) {
-        uint64_t i = n / y;
-        uint64_t j = n % y;
-        C_CPY(Fout[n], Fin[x * j + i]);
-    }
-}
-void
-FUNC_SSE(kfft_math_transpose_scalar)(const kfft_scalar* Fin, kfft_scalar* Fout, const uint32_t x,
-                                     const uint32_t y) {
-    for (uint64_t n = 0; n < x * y; n++) {
-        uint64_t i = n / y;
-        uint64_t j = n % y;
-        Fout[n] = Fin[x * j + i];
-    }
-}
-
-void
-FUNC_SSE(kfft_math_transpose_ip_cpx)(kfft_cpx* Fin, const uint32_t x, const uint32_t y) {
-    uint32_t r = y;
-    uint32_t c = x;
-
-    if (r <= 1 || c <= 1)
-        return;
-
-    for (uint32_t ind_last = r * c - 2; c > 1; --ind_last, --c) {
-        for (ssize_t i = r - 2; i != -1; --i) {
-            uint32_t ind = i * c + (c - 1);
-            kfft_cpx buf = {0, 0};
-            C_CPY(buf, Fin[ind]);
-            while (ind < ind_last) {
-                C_CPY(Fin[ind], Fin[ind + 1]);
-                ++ind;
-            }
-            C_CPY(Fin[ind_last], buf);
-            --ind_last;
-        }
-    }
-}
-void
-FUNC_SSE(kfft_math_transpose_ip_scalar)(kfft_scalar* Fin, const uint32_t x, const uint32_t y) {
-    uint32_t c = x, r = y;
-
-    if (r <= 1 || c <= 1)
-        return;
-
-    for (uint32_t ind_last = r * c - 2; c > 1; --ind_last, --c) {
-        for (ssize_t i = r - 2; i != -1; --i) {
-            uint32_t ind = i * c + (c - 1);
-            kfft_scalar buf = Fin[ind];
-            while (ind < ind_last) {
-                Fin[ind] = Fin[ind + 1];
-                ++ind;
-            }
-            Fin[ind_last] = buf;
-            --ind_last;
-        }
     }
 }
--- simd/sse/kfft_math_sse.h
+++ simd/sse/kfft_math_sse.h
@@ -2,32 +2,4 @@
 
 #include "kfft_types.h"
 
-static inline uint32_t
-FUNC_SSE(kfft_math_modpow)(uint32_t x, uint32_t y, uint32_t m) {
-    if (y == 0)
-        return 1;
-    uint64_t p = FUNC_SSE(kfft_math_modpow)(x, y / 2, m) % m;
-    p = (p * p) % m;
-
-    return (y % 2 == 0) ? (uint32_t)p : (uint32_t)((x * p) % m);
-}
-
-static inline uint32_t
-FUNC_SSE(kfft_math_gcd)(uint32_t a, uint32_t b) {
-    if (a == 0)
-        return b;
-    return FUNC_SSE(kfft_math_gcd)(b % a, a);
-}
-
-#if defined(KFFT_RADER_ALGO)
-uint32_t FUNC_SSE(kfft_math_prmn)(uint32_t num);
-uint32_t FUNC_SSE(kfft_math_prmni)(uint32_t a, uint32_t m);
-#endif /* KFFT_RADER_ALGO */
-
 void FUNC_SSE(kfft_math_adamar_cpx)(kfft_cpx* Fout, kfft_cpx* Fin, uint32_t size);
-void FUNC_SSE(kfft_math_transpose_cpx)(const kfft_cpx* Fin, kfft_cpx* Fout, const uint32_t x,
-                                       const uint32_t y);
-void FUNC_SSE(kfft_math_transpose_scalar)(const kfft_scalar* Fin, kfft_scalar* Fout,
-                                          const uint32_t x, const uint32_t y);
-void FUNC_SSE(kfft_math_transpose_ip_cpx)(kfft_cpx* Fin, const uint32_t x, const uint32_t y);
-void FUNC_SSE(kfft_math_transpose_ip_scalar)(kfft_scalar* Fin, const uint32_t x, const uint32_t y);
