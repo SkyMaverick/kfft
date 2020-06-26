f59cc93cdde4f67a571c2c459f83ad71451d7ff0
SkyMaverick <skymaverickas@gmail.com>
2020-06-26 10:48:33 +0300

    full half scalar SSE support now

--- simd/sse/half/kfft_generic.c
+++ simd/sse/half/kfft_generic.c
@@ -10,23 +10,23 @@ FUNC_SSE(std_method_eval)(kfft_cpx* Fout, kfft_cpx* Ftmp, const size_t fstride,
     uint32_t k = u, q1, q;
     __m128 T;
 
-    //    for (q1 = 0; q1 < p; ++q1, k += m)
-    //        _mm_store_ps((float*)&Ftmp[q1], _mm_load_ps((float*)&Fout[k]));
-    //
-    //    k = u;
-    //
-    //    for (q1 = 0; q1 < p; ++q1, k += m) {
-    //        uint32_t twidx = 0;
-    //        __m128 FOK = _mm_load_ps((float*)&Ftmp[0]);
-    //        for (q = 1; q < p; ++q) {
-    //            twidx += fstride * k;
-    //            if (twidx >= st->nfft)
-    //                twidx -= st->nfft;
-    //            kfft_cpx ctw = TWIDDLE(twidx, st);
-    //            C_MUL_SSE(T, _mm_load_ps((float*)&Ftmp[q]), _mm_load_ps((float*)&ctw));
-    //            C_ADD_SSE(FOK, FOK, T);
-    //            _mm_store_ps((float*)&Fout[k], FOK);
-    //        }
-    //    }
+    for (q1 = 0; q1 < p; ++q1, k += m)
+        _mm_storel_pi((__m64*)&Ftmp[q1], CLOAD1212(&Fout[k]));
+
+    k = u;
+
+    for (q1 = 0; q1 < p; ++q1, k += m) {
+        uint32_t twidx = 0;
+        __m128 FOK = _mm_load_ps((float*)&Ftmp[0]);
+        for (q = 1; q < p; ++q) {
+            twidx += fstride * k;
+            if (twidx >= st->nfft)
+                twidx -= st->nfft;
+            kfft_cpx ctw = TWIDDLE(twidx, st);
+            C_MUL_SSE(T, CLOAD1212(&Ftmp[q]), CLOAD1212(&ctw));
+            C_ADD_SSE(FOK, FOK, T);
+            _mm_storel_pi((__m64*)&Fout[k], FOK);
+        }
+    }
     return ret;
 }
--- simd/sse/half/kfft_math.c
+++ simd/sse/half/kfft_math.c
@@ -6,13 +6,12 @@
 void
 FUNC_SSE(kfft_math_adamar_cpx)(kfft_cpx* Fout, kfft_cpx* Fin, uint32_t size) {
     // FIXME Now primitive algorithm
-    //    kfft_math_adamar_cpx(Fout, Fin, size);
     __m128 Fo, Fi, T;
     for (uint32_t i = 0; i < size; i++) {
-        //        Fo = _mm_load_ps((float*)&Fout[i]);
-        //        /* FIXME if use _mm_load_ps debug build crash with SIGSEGV */
-        //        Fi = _mm_set_ps((float)Fin[i].i, (float)Fin[i].r);
-        //        C_MUL_SSE(T, Fo, Fi);
-        //        _mm_store_ps((float*)&Fout[i], T);
+        Fo = CLOAD1212(&Fout[i]);
+        /* FIXME if use _mm_load_ps debug build crash with SIGSEGV */
+        Fi = _mm_set_ps((float)Fin[i].i, (float)Fin[i].r, (float)Fin[i].i, (float)Fin[i].r);
+        C_MUL_SSE(T, Fo, Fi);
+        _mm_storel_pi((__m64*)&Fout[i], T);
     }
 }
