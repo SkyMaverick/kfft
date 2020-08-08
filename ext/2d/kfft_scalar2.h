acbab0c0ba914911aa0807ec1cbf6076d6a8c5a3
SkyMaverick <skymaverickas@gmail.com>
2020-08-08 20:28:24 +0300

    More naming fixes

--- ext/2d/kfft_scalar2.h
+++ ext/2d/kfft_scalar2.h
@@ -14,11 +14,11 @@ KFFT_API kfft_plan_s2d*
 kfft_config2_scalar(const uint32_t x_size, const uint32_t y_size, const uint32_t flags,
                     kfft_pool_t* A, size_t* lenmem);
 KFFT_API kfft_return_t
-kfft_eval2_scalar(kfft_plan_s2d* cfg, const kfft_scalar* fin, kfft_cpx* fout);
+kfft_eval2_scalar(kfft_plan_s2d* plan, const kfft_scalar* fin, kfft_cpx* fout);
 KFFT_API kfft_return_t
-kfft_eval2_scalar_norm(kfft_plan_s2d* cfg, const kfft_scalar* fin, kfft_scalar* fout);
+kfft_eval2_scalar_norm(kfft_plan_s2d* plan, const kfft_scalar* fin, kfft_scalar* fout);
 KFFT_API kfft_return_t
-kfft_evali2_scalar(kfft_plan_s2d* cfg, const kfft_cpx* fin, kfft_scalar* fout);
+kfft_evali2_scalar(kfft_plan_s2d* plan, const kfft_cpx* fin, kfft_scalar* fout);
 KFFT_API void
 kfft_shift2_scalar(kfft_scalar* buf, kfft_scalar* ftmp, const uint32_t sz_x, const uint32_t sz_y,
                    const bool is_inverse, kfft_pool_t* mmgr);
@@ -29,11 +29,11 @@ typedef kfft_plan_s2d*
 (*kfft_callback_config2_scalar)(const uint32_t x_size, const uint32_t y_size,
                                 const uint32_t flags, kfft_pool_t* A, size_t* lenmem);
 typedef kfft_return_t
-(*kfft_callback_eval2_scalar)(kfft_plan_s2d* cfg, const kfft_scalar* fin, kfft_cpx* fout);
+(*kfft_callback_eval2_scalar)(kfft_plan_s2d* plan, const kfft_scalar* fin, kfft_cpx* fout);
 typedef kfft_return_t
-(*kfft_callback_eval2_scalar_norm)(kfft_plan_s2d* cfg, const kfft_scalar* fin, kfft_scalar* fout);
+(*kfft_callback_eval2_scalar_norm)(kfft_plan_s2d* plan, const kfft_scalar* fin, kfft_scalar* fout);
 typedef kfft_return_t
-(*kfft_callback_evali2_scalar)(kfft_plan_s2d* cfg, const kfft_cpx* fin, kfft_scalar* fout);
+(*kfft_callback_evali2_scalar)(kfft_plan_s2d* plan, const kfft_cpx* fin, kfft_scalar* fout);
 typedef void
 (*kfft_callback_shift2_scalar)(kfft_scalar* buf, kfft_scalar* ftmp, const uint32_t sz_x,
                                const uint32_t sz_y, const bool is_inverse, kfft_pool_t* mmgr);
--- incs/kfft_algo.h
+++ incs/kfft_algo.h
@@ -1,27 +1,27 @@
-#define KFFT_ALGO_PLAN_PREPARE(st, flags, type, memneed, pool, len_value)                          \
+#define KFFT_ALGO_PLAN_PREPARE(plan, flags, type, memneed, pool, len_value)                          \
     do {                                                                                           \
         kfft_pool_t* mm_##type = NULL;                                                             \
         if (lenmem == NULL) {                                                                      \
             mm_##type = (pool) ? pool : kfft_pool_create(memneed);                                 \
             if (mm_##type)                                                                         \
-                st = kfft_pool_alloc(mm_##type, sizeof(type));                                     \
-            if ((st == NULL) && (pool == NULL))                                                    \
+                plan = kfft_pool_alloc(mm_##type, sizeof(type));                                     \
+            if ((plan == NULL) && (pool == NULL))                                                    \
                 kfft_pool_free(mm_##type);                                                         \
         } else {                                                                                   \
             if (pool && *len_value >= (memneed)) {                                                 \
                 mm_##type = pool;                                                                  \
                 if ((flags)&KFFT_FLAG_RENEW)                                                       \
                     kfft_pool_clear(mm_##type);                                                    \
-                st = kfft_pool_alloc(mm_##type, sizeof(type));                                     \
+                plan = kfft_pool_alloc(mm_##type, sizeof(type));                                     \
             }                                                                                      \
             *len_value = (memneed);                                                                \
         }                                                                                          \
-        if (st)                                                                                    \
-            st->object.mmgr = mm_##type;                                                           \
+        if (plan)                                                                                    \
+            plan->object.mmgr = mm_##type;                                                           \
     } while (0)
 
-#define KFFT_ALGO_PLAN_TERMINATE(st, pool)                                                         \
+#define KFFT_ALGO_PLAN_TERMINATE(plan, pool)                                                         \
     do {                                                                                           \
         if ((pool) == NULL)                                                                        \
-            kfft_pool_free(KFFT_PLAN_MMGR(st));                                                    \
+            kfft_pool_free(KFFT_PLAN_MMGR(plan));                                                    \
     } while (0)
