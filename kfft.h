fe7c2e0b6f0cd883f93c8d6eac3526af89c92723
SkyMaverick <skymaverickas@gmail.com>
2020-08-24 22:43:04 +0300

    some format

--- kfft.h
+++ kfft.h
@@ -7,41 +7,43 @@
  */
 
 /*!
- * @mainpage
- *
- * \author Alexander Smirnov (https://github.com/SkyMaverick)
- * \version 0.7.1
- *
- * LibKFFT is a free/open-source (ZLib licensed) C library
- * providing fast fourier transform and other related operations
- * for comlex and scalar double or float sequences.
- * LibKFFT is deeply redesigned fork KissFFT project by Mark Borgerding
- * (https://github.com/mborgerding/kissfft).
- *
- * See the LibKFFT page on GitHub for downloads and other information,
- * or the source code (https://github.com/SkyMaverick/kfft).
- *
- * The features of LibKFFT include:
- * - complex and scalar fourier transformation operations (forward and inverse)
- *      - 64-bit float type (double) as default
- *      - 32-bit float type (float) if set option KFFT_HALF_SCALAR
- * - SIMD acceleration transform operations (now support SSE, SSE2, SSE3 (optional)) (build option)
- * - Recursive Rader algorithm (https://en.wikipedia.org/wiki/Rader's_FFT_algorithm) for
- * prime-number lenght sequenses (build option)
- * - Internal math functions (aka sqrt, sincos etc.) without system libm functions (build
- * option)
- * - Parallelization with OpenMP framework (build option)
- * - 2D, sparse, convolution(+2D) operations as build-time extensions
- * - Detail trace log in stdout for diagnostic (build option)
- * - Tiny API support for simplify code in trivial usage
- *
- * Test build on OS with CC:
- * - Linux (x86-64): GCC 9.3, Clang-10
- * - Windows (x86-64): MSVC 2015, 2017, Clang-10
- * - RPi3 (armhf): GCC 8.3
- */
+   \mainpage
 
-/** @file */
+   \author Alexander Smirnov (https://github.com/SkyMaverick)
+   \version 0.7.1
+
+   LibKFFT is a free/open-source (ZLib licensed) C library
+   providing fast fourier transform and other related operations
+   for comlex and scalar double or float sequences.
+   ![Fourier transform formula](fft.svg)
+
+   LibKFFT is deeply redesigned fork KissFFT project by Mark Borgerding
+   (https://github.com/mborgerding/kissfft).
+
+   See the LibKFFT page on GitHub for downloads and other information,
+   or the source code (https://github.com/SkyMaverick/kfft).
+
+   The features of LibKFFT include:
+   - complex and scalar fourier transformation operations (forward and inverse)
+        - 64-bit float type (double) as default
+        - 32-bit float type (float) if set option KFFT_HALF_SCALAR
+   - SIMD acceleration transform operations (now support SSE, SSE2, SSE3 (optional)) (build option)
+   - Recursive Rader algorithm (https://en.wikipedia.org/wiki/Rader's_FFT_algorithm) for
+   prime-number lenght sequenses (build option)
+   - Internal math functions (aka sqrt, sincos etc.) without system libm functions (build
+   option)
+   - Parallelization with OpenMP framework (build option)
+   - 2D, sparse, convolution(+2D) operations as build-time extensions
+   - Detail trace log in stdout for diagnostic (build option)
+   - Tiny API support for simplify code in trivial usage
+
+   Test build on OS with CC:
+   - Linux (x86-64): GCC 9.3, Clang-10
+   - Windows (x86-64): MSVC 2015, 2017, Clang-10
+   - RPi3 (armhf): GCC 8.3
+
+   \file
+ */
 
 #pragma once
 
