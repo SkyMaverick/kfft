877a9180dd44525c4de8a1e6ffa1cff3503ae4e3
SkyMaverick <skymaverickas@gmail.com>
2020-08-24 13:32:59 +0300

    more documentation in kfft_tiny.h

---
+++ docs/example/example_tiny.c
@@ -0,0 +1,110 @@
+#include "kfft_tiny.h"
+#include <stdio.h>
+
+#define KFFT_ARRAY_SIZE 100
+
+#define KFFT_ARRAY_X 50
+#define KFFT_ARRAY_Y 50
+#define KFFT_ARRAY_SIZE2 ((KFFT_ARRAY_X) * (KFFT_ARRAY_Y))
+
+/* Complex 2D convolution example */
+static kfft_return_t
+kfft_complex_conv_2d (void) {
+    kfft_return_t ret = KFFT_RET_ALLOC_FAIL;
+    kfft_cpx* in1, *in2, *out;
+    /*
+       It is allowed to use one buffer,
+       but kfft_evaluation() will allocate an equivalent temporary buffer.
+     */
+    in1 = in2 = out = kfft_malloc (3 * KFFT_ARRAY_SIZE2 * sizeof(kfft_cpx));
+    if (out) {
+        in1 += KFFT_ARRAY_SIZE2;
+        in2 += 2 * KFFT_ARRAY_SIZE2;
+
+        /* Fill complex data to IN buffer HERE !!! */
+
+        kfft_args_2d A = {KFFT_ARRAY_X, KFFT_ARRAY_Y};
+        kfft_plan plan = kfft_configuration(KFFT_PLAN_COMPLEX_CONV2D, KFFT_FLAG_NORMAL, &A);
+        if (plan) {
+            ret = kfft_convolution(plan, in1, in2, out);
+            
+            /* STDOUT complex data contains OUT buffer HERE !!! */
+            
+            kfft_finalization(plan);
+        }
+        kfft_free(out);
+    }
+    return ret;
+
+}
+
+/* Scalar 1D buffer evaluate example */
+static kfft_return_t
+kfft_scalar_1d (void) {
+    kfft_return_t ret = KFFT_RET_ALLOC_FAIL;
+    kfft_scalar* in  = kfft_malloc (KFFT_ARRAY_SIZE * sizeof(kfft_scalar));
+    kfft_cpx*    out = kfft_malloc (KFFT_ARRAY_SIZE * sizeof(kfft_cpx));
+    
+//    in = kfft_malloc (KFFT_ARRAY_SIZE * sizeof(kfft_cpx) + sizeof(kfft_scalar));
+    if (in && out) {
+//        out = (kfft_cpx*)(in + KFFT_ARRAY_SIZE);
+
+        /* Fill complex data to IN buffer HERE !!! */
+
+        size_t A = KFFT_ARRAY_SIZE;
+        kfft_plan plan = kfft_configuration(KFFT_PLAN_SCALAR, KFFT_FLAG_NORMAL, &A);
+        if (plan) {
+            ret = kfft_evaluation(plan, in, out);
+            
+            /* STDOUT complex data contains OUT buffer HERE !!! */
+            
+            kfft_finalization(plan);
+        }
+        kfft_free(in);
+        kfft_free(out);
+    }
+    return ret;
+}
+
+/* Complex 1D buffer evaluate example */
+static kfft_return_t
+kfft_complex_1d (void) {
+    kfft_return_t ret = KFFT_RET_ALLOC_FAIL;
+    kfft_cpx* in, *out;
+    /*
+       It is allowed to use one buffer,
+       but kfft_evaluation() will allocate an equivalent temporary buffer.
+     */
+    in = out = kfft_malloc (2 * KFFT_ARRAY_SIZE * sizeof(kfft_cpx));
+    if (out) {
+        in += KFFT_ARRAY_SIZE;
+
+        /* Fill complex data to IN buffer HERE !!! */
+
+        size_t A = KFFT_ARRAY_SIZE;
+        kfft_plan plan = kfft_configuration(KFFT_PLAN_COMPLEX, KFFT_FLAG_NORMAL, &A);
+        if (plan) {
+            ret = kfft_evaluation(plan, in, out);
+            
+            /* STDOUT complex data contains OUT buffer HERE !!! */
+            
+            kfft_finalization(plan);
+        }
+        kfft_free(out);
+    }
+    return ret;
+}
+
+int 
+main () {
+    kfft_return_t ret = KFFT_RET_SUCCESS;
+
+    if ((ret = kfft_complex_1d()) != KFFT_RET_SUCCESS)
+        fprintf(stderr, "%s\n", kfft_strerr(ret));
+    
+    if ((ret = kfft_scalar_1d()) != KFFT_RET_SUCCESS)
+        fprintf(stderr, "%s\n", kfft_strerr(ret));
+    
+    if ((ret = kfft_complex_conv_2d()) != KFFT_RET_SUCCESS)
+        fprintf(stderr, "%s\n", kfft_strerr(ret));
+}
--- kfft_tiny.h
+++ kfft_tiny.h
@@ -8,6 +8,10 @@
     Used when there is no need for flexible manipulation of memory and plan objects.
  */
 
+/*! \example example_tiny.c
+    Tiny API usage example for any data buffers.
+ */
+
 #include "kfft.h"
 
 #ifdef __cplusplus
@@ -50,6 +54,7 @@ typedef kfft_plan_t* kfft_plan;
 #define KTINY_CPX(X) (kfft_cpx*)(X)
 /// Cast any pointer to ::kfft_scalar pointer
 #define KTINY_SCLR(X) (kfft_scalar*)(X)
+
 /*! Amalgamited standart config arguments type. @see kfft_config_cpx */
 typedef struct {
     uint32_t nfft; ///< sequencce lenght
@@ -67,6 +72,56 @@ typedef struct {
     uint32_t dims; ///< columns count
     uint32_t step; ///< step size
 } kfft_args_sparse;
+
+/*!
+    Amalgamited kfft_confg_* functions.
+
+    \param[in] T - tiny plan type ::ktiny_plan_type
+    \param[in] F - config flags ::kfft_eval_mods
+    \param[in] A - unify pointer for kfft_args_* structure
+    \result - configured untiped plan ::kfft_plan
+
+    \note Flag ::KFFT_FLAG_RENEW ignored
+    \warning This function allocates memory. Must to use ::kfft_finalization() to relase this
+   memory.
+ */
+#define kfft_configuration(T, F, A) kfft_tiny_config((T), (F), (uintptr_t)(A))
+
+/*!
+    Amalgamited kfft_eval_* functions.
+
+    \param[in] P - ::kfft_plan unify pointer
+    \param[in] Fi - input sequence pointer
+    \param[in] Fo - output sequence pointer
+    \result - return status info ::kfft_return_t
+
+    \note output buffer must be allocation with ::kfft_malloc or ::KFFT_MALLOC functions.
+    \warning The function does not check the type and correctness of the data buffer.
+ */
+#define kfft_evaluation(P, Fi, Fo) kfft_tiny_eval(P, (kfft_scalar*)(Fi), (kfft_scalar*)(Fo))
+
+/*!
+    Amalgamited convolution functions.
+
+    \param[in] P - ::kfft_plan unify pointer
+    \param[in] Fi1 - input sequence pointer
+    \param[in] Fi2 - input sequence pointer
+    \param[in] Fo - output sequence pointer
+    \result - return status info ::kfft_return_t
+
+    \note output buffer must be allocation with ::kfft_malloc or ::KFFT_MALLOC functions.
+    \warning The function does not check the type and correctness of the data buffer.
+ */
+#define kfft_convolution(P, Fi1, Fi2, Fo)                                                                \
+    kfft_tiny_conv(P, (kfft_scalar*)(Fi1), (kfft_scalar*)(Fi2), (kfft_scalar*)(Fo))
+
+/*!
+    Cleanup and free ::kfft_plan. NULL input pointer.
+
+    \param[in] P - ::kfft_plan
+    \result - None
+ */
+#define kfft_finalization(P) kfft_tiny_release(&(P))
 
 static kfft_plan KFFT_UNUSED_FUNC
 kfft_tiny_config(unsigned type, uint32_t flags, uintptr_t args) {
@@ -154,20 +209,6 @@ kfft_tiny_config(unsigned type, uint32_t flags, uintptr_t args) {
     return plan;
 }
 
-/*!
-    Amalgamited kfft_confg_* functions.
-
-    \param[in] T - tiny plan type ::ktiny_plan_type
-    \param[in] F - config flags ::kfft_eval_mods
-    \param[in] A - unify pointer for kfft_args_* structure
-    \result - configured untiped plan ::kfft_plan
-
-    \note Flag ::KFFT_FLAG_RENEW ignored
-    \warning This function allocates memory. Must to use ::kfft_finalization() to relase this
-   memory.
- */
-#define kfft_configuration(T, F, A) kfft_tiny_config((T), (F), (uintptr_t)(A))
-
 static kfft_return_t KFFT_UNUSED_FUNC
 kfft_tiny_eval(const kfft_plan plan, const kfft_scalar* fin, kfft_scalar* fout) {
     switch (plan->type) {
@@ -218,31 +259,19 @@ kfft_tiny_eval(const kfft_plan plan, const kfft_scalar* fin, kfft_scalar* fout) 
     return KFFT_RET_IMPROPER_PLAN;
 }
 
-/*!
-    Amalgamited kfft_eval_* functions.
-
-    \param[in] - P ::kfft_plan unify pointer
-    \param[in] - Fi input sequence pointer
-    \param[in] - Fo output sequence pointer
-        \note output buffer must be allocation with ::kfft_malloc or ::KFFT_MALLOC functions.
-    \result - return status info ::kfft_return_t
-
-    \warning The function does not check the type and correctness of the data buffer.
- */
-#define kfft_evalation(P, Fi, Fo) kfft_tiny_eval(P, (kfft_scalar*)(Fi), (kfft_scalar*)(Fo))
 
 static kfft_return_t KFFT_UNUSED_FUNC
 kfft_tiny_conv(const kfft_plan plan, const kfft_scalar* fin1, const kfft_scalar* fin2,
                kfft_scalar* fout) {
     switch (plan->type) {
-#if defined (KFFT_CONV_ENABLE)
+#if defined(KFFT_CONV_ENABLE)
     case KFFT_PLAN_COMPLEX_CONV:
         return kfft_eval_conv_cpx(KTINY_CAST(ccnv, plan), KTINY_CPX(fin1), KTINY_CPX(fin2),
                                   KTINY_CPX(fout));
     case KFFT_PLAN_SCALAR_CONV:
         return kfft_eval_conv_scalar(KTINY_CAST(scnv, plan), fin1, fin2, fout);
 #endif /* KFFT_CONV_ENABLE */
-#if defined (KFFT_CONV2D_ENABLE)
+#if defined(KFFT_CONV2D_ENABLE)
     case KFFT_PLAN_COMPLEX_CONV2D:
         return kfft_eval2_conv_cpx(KTINY_CAST(c2cnv, plan), KTINY_CPX(fin1), KTINY_CPX(fin2),
                                    KTINY_CPX(fout));
@@ -253,35 +282,11 @@ kfft_tiny_conv(const kfft_plan plan, const kfft_scalar* fin1, const kfft_scalar*
     return KFFT_RET_IMPROPER_PLAN;
 }
 
-/*!
-    Amalgamited convolution functions.
-
-    \param[in] - P ::kfft_plan unify pointer
-    \param[in] - Fi1 input sequence pointer
-    \param[in] - Fi2 input sequence pointer
-    \param[in] - Fo output sequence pointer
-        \note output buffer must be allocation with ::kfft_malloc or ::KFFT_MALLOC functions.
-
-    \result - return status info ::kfft_return_t
-
-    \warning The function does not check the type and correctness of the data buffer.
- */
-#define kfft_convolution(P, Fi, Fo)                                                                \
-    kfft_tiny_conv(P, (kfft_scalar*)(Fi1), (kfft_scalar*)(Fi2), (kfft_scalar*)(Fo))
-
 static void KFFT_UNUSED_FUNC
 kfft_tiny_release(kfft_plan* plan) {
     kfft_cleanup((void*)((*plan)->state));
     kfft_free(*plan);
 }
-
-/*!
-    Cleanup and free ::kfft_plan. NULL input pointer.
-
-    \param[in] - P ::kfft_plan
-    \result - None
- */
-#define kfft_finalization(P) kfft_tiny_release(&(P))
 
 #ifdef __cplusplus
 }
