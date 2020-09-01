1b4546481bdbf61b84e06ee1ab427bdd7a6978ae
SkyMaverick <skymaverickas@gmail.com>
2020-09-01 15:09:31 +0300

    bump version

--- kfft.h
+++ kfft.h
@@ -10,7 +10,7 @@
    \mainpage
 
    \author Alexander Smirnov (https://github.com/SkyMaverick)
-   \version 0.7.1
+   \version 0.8.0
 
    LibKFFT is a free/open-source (ZLib licensed) C library
    providing fast fourier transform and other related operations
